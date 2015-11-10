2e98218b650869b9220a6ef46212284b7327ed4f
Alex Branham <branham@utexas.edu>
2015-11-10 15:07:52 -0600

    slight updates

--- 11-11-section-code.R
+++ 11-11-section-code.R
@@ -26,3 +26,25 @@ t.test(mydata$x, mydata$x2)
 
 # making tables
 with(mydata, table(y, z))
+
+# note that we can create our own functions
+# useful if R doesn't have what you need already:
+# for example, power calculation from class yesterday:
+
+my_power_function <- function(n){
+  pnorm(120 - 1.64 * sqrt(90/n) - 118) / sqrt(90/n)
+}
+
+
+# Reading data into R:
+# Often we use data that isn't stored as an R object
+# (e.g. stat files or excel spreadsheets)
+# Here are some useful packages/functions for getting such data into R:
+
+# Stata, spss
+# foreign - read.dta() or read.spss() ## note only older Stata files
+# haven - read_dta() ## works for all stata files
+
+
+# other formats:
+# https://support.rstudio.com/hc/en-us/articles/201057987-Quick-list-of-useful-R-packages
